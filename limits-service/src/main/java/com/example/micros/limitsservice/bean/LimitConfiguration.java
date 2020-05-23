05382241a01afed1321db2cbd6816bdb15fbaaf5
Jyothi Suhani <jyothisuhanip009@gmail.com>
2020-05-23 10:22:46 +0530

    limits microservice

---
+++ limits-service/src/main/java/com/example/micros/limitsservice/bean/LimitConfiguration.java
@@ -0,0 +1,27 @@
+package com.example.micros.limitsservice.bean;
+
+public class LimitConfiguration {
+
+	int maximum;
+	int minimum;
+	
+	protected LimitConfiguration() {
+		
+	}
+	
+	public LimitConfiguration(int maximum, int minimum) {
+		super();
+		this.maximum = maximum;
+		this.minimum = minimum;
+	}
+	public int getMaximum() {
+		return maximum;
+	}
+	public int getMinimum() {
+		return minimum;
+	}
+	
+	
+	
+	
+}
