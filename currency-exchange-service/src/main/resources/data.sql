aecdb5a14ec0ba8b48e550e3da8c4a9695aadcc5
Jyothi Suhani <jyothisuhanip009@gmail.com>
2020-05-23 15:05:35 +0530

    currency exchange service and adding jpa

---
+++ currency-exchange-service/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+Insert into exchange_value(currency_from,currency_to,id,conversion_multiple,port)
+values('USD','INR',10001,65,0);
+Insert into exchange_value(currency_from,currency_to,id,conversion_multiple,port)
+values('EUR','INR',10002,75,0);
+Insert into exchange_value(currency_from,currency_to,id,conversion_multiple,port)
+values('AUR','INR',10003,25,0);
